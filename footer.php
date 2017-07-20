03113cd363b5bd54bb0cb8ce2e07717432a5f00f
ambodin <am.bodin@hotmail.fr>
2017-07-20 15:34:25 +0200

    Modification du merveilleux footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 				<!-- Footer -->
 					<footer id="footer">
 						<p class="copyright"><?php dimension_footer_text()?>web work: <a href="http://cog.dog">cog.dog</a> &bull; theme: <a href="https://github.com/cogdog/wp-dimension">WP Dimension</a> based on <a href="https://html5up.net/dimension">HTML5 UP</a></p>
+						<ul>
+							<li style="color:pink;"><a href="https://www.youtube.com/watch?v=LtyPEc2t18k" style="text-decoration:none;">Popopo</a></li>
+							<li style="color:purple;"><a href="https://www.youtube.com/watch?v=kJQP7kiw5Fk" style="text-decoration:none;">Bon son</a></li>
+						</ul>
 					</footer>
 
 			</div> <!-- Wrapper -->
@@ -8,9 +12,7 @@
 		<!-- BG -->
 			<div id="bg"></div>
 
-			
+
 	<?php wp_footer(); ?>
 	</body>
 </html>
-
-
