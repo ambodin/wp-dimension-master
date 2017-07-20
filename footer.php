678e91a46149e84d54e325b50c3b21a0e8966bae
ambodin <am.bodin@hotmail.fr>
2017-07-20 15:41:49 +0200

    Déplacement styles inline dans le fichier main.css

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 					<footer id="footer">
 						<p class="copyright"><?php dimension_footer_text()?>web work: <a href="http://cog.dog">cog.dog</a> &bull; theme: <a href="https://github.com/cogdog/wp-dimension">WP Dimension</a> based on <a href="https://html5up.net/dimension">HTML5 UP</a></p>
 						<ul>
-							<li style="color:pink;"><a href="https://www.youtube.com/watch?v=LtyPEc2t18k" style="text-decoration:none;">Popopo</a></li>
-							<li style="color:purple;"><a href="https://www.youtube.com/watch?v=kJQP7kiw5Fk" style="text-decoration:none;">Bon son</a></li>
+							<li><a href="https://www.youtube.com/watch?v=LtyPEc2t18k">Popopo</a></li>
+							<li><a href="https://www.youtube.com/watch?v=kJQP7kiw5Fk">Bon son</a></li>
 						</ul>
 					</footer>
 
